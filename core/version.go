37167c27463a1dfbd7a2704907b89449ef397117
Sergey Zhuravlev <sergey@netangels.ru>
2026-02-23 11:48:51 +0500

    Version 2.0.2

--- core/version.go
+++ core/version.go
@@ -2,4 +2,4 @@ package core
 
 import "github.com/0xef53/phoenix-guest-agent/internal/version"
 
-var AgentVersion = version.Version{Major: 2, Minor: 0, Micro: 1}
+var AgentVersion = version.Version{Major: 2, Minor: 0, Micro: 2}
